a9d76daae5ed5bc8fe02e0cf3538674e19e7dbf7
Steven Bell <steven.bell@hbo.com>
2017-06-08 08:10:17 -0700

    First blog post.

--- src/components/blog/blogIndex.data.js
+++ src/components/blog/blogIndex.data.js
@@ -1,7 +1,7 @@
 export default [
 {
   id: 1,
-  title: 'Building this site. Part 1',
+  title: 'Let\'s get it (this blog) started!',
   publishTimestamp: 1496300400000,
   path: '2017/06/01',
   shortDescription: 'Explore the creation of this site. In this first blog post we\'ll explore why I created this site and some of the technology I\'ve selected.'
