e99801ae2650c82a651bfc7fa316dd783462c99d
Steven Bell <steven.bell@hbo.com>
2017-06-09 11:14:48 -0700

    add jshintrc. Add metatdata for next blog post.

--- src/components/blog/blogIndex.data.js
+++ src/components/blog/blogIndex.data.js
@@ -1,10 +1,10 @@
 export default [
 {
   id: 2,
-  title: '',
+  title: 'Sublime? Atom? Not when I have Vim.',
   publishTimestamp: 1498892400000,
   path: '2017/07/01',
-  shortDescription: ''
+  shortDescription: 'An introduction into some of the features of vim. Why I use it as my editor of choice when writing code, and how (easy it is) to setup vim. There is a reason this age old editor is still one of the more popular text editors. Oh, and of course, emacs sucks...'
 },
 {
   id: 1,
@@ -13,4 +13,4 @@ export default [
   path: '2017/06/01',
   shortDescription: 'Explore the creation of this site. In this first blog post we\'ll explore why I created this site and some of the technology I\'ve selected.'
 }
-]
+];
