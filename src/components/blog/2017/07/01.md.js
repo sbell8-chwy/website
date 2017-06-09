10892d841915ebd3978b6bf4c01f342208af855c
Steven Bell <steven.bell@hbo.com>
2017-06-09 11:56:07 -0700

    Starting blog.

--- src/components/blog/2017/07/01.md.js
+++ src/components/blog/2017/07/01.md.js
@@ -1,2 +1,5 @@
 export default `
-`
+### Sublime? Atom? Not when I have Vim.
+
+According to the internet vim is still one of the more popular text editing programs around but in my professional life I still get a lot of raised eyebrows and sideways glances when I tell people I do nearly all my development work in vim.
+`;
